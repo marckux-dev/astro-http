66d69a96a721c00122a1fb1e7d6efd11bcbccc7e
marckux-dev <marckux.developer@gmail.com>
2026-01-05 15:54:15 +0100

    Añado cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,21 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
+
+// import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
-  output: 'server',
+
+  output: 'server'
+  /*
   adapter: node({
     mode: 'standalone',
   }),
+  */,
+
+  adapter: cloudflare()
 });
